6036d4e7a4480208024085deb7d3818387f5a289
ihor <mr.ihor.lyzak@gmail.com>
2021-12-30 15:21:27 +0200

    well organized file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Main from '../Main/Main';
+// import Main from 'components/Main/Main';
 
 function App() {
   return (
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './cardList.module.scss';
+// import styles from 'components/cardList.module.scss';
 import Card from '../Card/Card';
-
+// import Card from 'components/Card/Card';
 
 
 const CardList = () => {
