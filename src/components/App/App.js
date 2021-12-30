5e817c80ee1ac5e406b70365887dab973d04d9e7
ihor <mr.ihor.lyzak@gmail.com>
2021-12-30 19:20:41 +0200

    adding cities function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,30 @@
-import './App.css';
-import { Main } from '../Main/Main';
+import React, { useState } from 'react';
+import { Input } from '../Input/Input';
+import { Card } from '../Card/Card';
+
+import './App.scss';
+
+
 
 function App() {
 
+  const [citiesList, setCitiesList] = useState([]);
+
   return (
+    <div className='mainBackground'>
 
-    <Main />
+      <div className='main'>
+        <Input setCitiesList={setCitiesList} />        
 
+        <div className='cardList'>
+          {
+            citiesList.map(city => <Card key={city} city={city} />)
+          }
+        </div>
+
+      </div>
+
+    </div>
   );
 }
 
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import styles from './input.module.scss';
 
-export const Input = () => {
+
+
+export const Input = ({ setCitiesList }) => {
+    const [inputValue, setInputValue] = useState('empty');
+
+    const handleOnClick = () => {
+        setCitiesList((currentArray) => [...currentArray, inputValue])
+    }
+
+    const handleOnChange = (e) => {
+        setInputValue(e.target.value); //changing state by using e.target.value
+    }
+
     return (
         <div className={styles.inputWrap}>
-            <input placeholder='Enter the city name' className={styles.input}></input>
-            <button className={styles.button}>Add city</button>
+            <input value={inputValue} className={styles.input} onChange={handleOnChange}></input>
+            <button onClick={handleOnClick} className={styles.button}>Add city</button>
         </div>
-    );
+    )
 }
